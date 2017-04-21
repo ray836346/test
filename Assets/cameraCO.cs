15dedb3b0b7f0b60fca38d0f67c12c7bcfb5a4a9
ray836346 <ricky159357@gmail.com>
2017-04-21 11:36:02 +0800

    demo
    demo

---
+++ Assets/cameraCO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//攝影機追蹤
+
+public class cameraCO : MonoBehaviour {
+
+	private Vector3 offset;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = player.transform.position + offset;
+
+	}
+}
+
+
---
+++ Assets/player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public Text winText;
+	private float score;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody>();
+		score = 0;
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	//移動
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 move = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (move * speed);
+	}
+
+	//碰撞時方快消失
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("pick")) {
+			other.gameObject.SetActive(false);
+			score = score+1;
+			if(score>=3){   //皆消失後即勝利
+				winText.text = "勝利";
+			}
+		}
+	}
+
+
+}
